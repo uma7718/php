31a53a6767704be7049f399c724251523f74daaf
yuma kurano <neutral5718@gmail.com>
2021-09-12 16:14:20 +0900

    2-2-8を追加

--- index.php
+++ index.php
@@ -142,3 +142,19 @@ EOM;
 </p>
 
 <!-- 2-2-8 -->
+<p>
+  <?php
+  $airports = ['Haneda', 'Narita', 'Chubu'];
+  $airports[] = 'Kansai';
+  $airports[] = 'Naha';
+  
+  $airports[2] = 'Sendai'
+  ?>
+  <p><?=$airports[0]?></p>
+  <p><?=$airports[1]?></p>
+  <p><?=$airports[2]?></p>
+  <p><?=$airports[3]?></p>
+  <p><pre><?php print_r($airports);?></pre></p>
+</p>
+
+<!-- 2-2-9 -->
